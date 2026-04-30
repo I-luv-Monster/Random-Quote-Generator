98d31971b8606cedb8ac4537388d6add631af0b9
I-luv-Monster <abrahamavalenzuelaiii@gmail.com>
2026-04-30 21:51:22 +0000

    hjksadgfjasgdfhjgasd.kfhjak.sjdfk.jZSDHF:uaesf

---
+++ script.js
@@ -0,0 +1,7 @@
+let quotes = [
+    {text: "The only way to do great work is to love what you do.", author: "Steve Jobs"},
+    // Add 4-5 more quotes here
+    {text: "Man has gone out to explore other worlds and other civilizations without having explored his own labyrinth of dark passages and secret chambers, and without finding what lies behind doorways that he himself has sealed.", author: "Stanisław Lem, Solaris."}
+    {}
+    
+];
