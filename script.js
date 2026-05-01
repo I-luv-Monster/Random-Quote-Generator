511edd32bdc157356438cf9be8c279a79e829a3f
I-luv-Monster <abrahamavalenzuelaiii@gmail.com>
2026-05-01 22:04:20 +0000

    zxCvasvsvbvZFDBzdfb

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let quotes = [
     {text: "The only way to do great work is to love what you do.", author: "Steve Jobs"},
     // Add 4-5 more quotes here
     {text: "Man has gone out to explore other worlds and other civilizations without having explored his own labyrinth of dark passages and secret chambers, and without finding what lies behind doorways that he himself has sealed.", author: "Stanisław Lem, Solaris."}
-    {}
-    
+    { "For mankind to achieve greatness they shell Unify as One.", author: "Unknown"},
+    { "", author: "Unknown"},
+
 ];
+
+function displayRandomQuote() {
+    // Make the button work when clicked
+document.getElementById("new-quote-btn").addEventListener("click", displayRandomQuote);
+
+    // Your code will go here to:
+    // 1. Pick a random number
+    // 2. Get a quote from the array
+    // 3. Display it on the page
+}
+
