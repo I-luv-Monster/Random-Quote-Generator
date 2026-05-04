f643f0a9542d3654b5ea34797c642dffac91ab3e
I-luv-Monster <abrahamavalenzuelaiii@gmail.com>
2026-05-04 21:54:41 +0000

    argqwgadsgd

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let quotes = [
     {text: "The only way to do great work is to love what you do.", author: "Steve Jobs"},
     // Add 4-5 more quotes here
-    {text: "Man has gone out to explore other worlds and other civilizations without having explored his own labyrinth of dark passages and secret chambers, and without finding what lies behind doorways that he himself has sealed.", author: "Stanisław Lem, Solaris."}
-    { "For mankind to achieve greatness they shell Unify as One.", author: "Unknown"},
-    { "", author: "Unknown"},
+    {text: "Man has gone out to explore other worlds and other civilizations without having explored his own labyrinth of dark passages and secret chambers, and without finding what lies behind doorways that he himself has sealed.", author: "Stanisław Lem, Solaris."},
+    {text: "For mankind to achieve greatness they shell Unify as One.", author: "Unknown"},
+    {text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt"}
 
 ];
 
